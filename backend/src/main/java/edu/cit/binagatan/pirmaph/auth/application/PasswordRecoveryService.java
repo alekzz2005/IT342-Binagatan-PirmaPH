846eb59914d51052fe3683bc7a5c182665fe2803
alekzz2005 <binagatanalexander2005@gmail.com>
2026-05-24 10:34:49 +0800

    chore(email): remove spring-boot-starter-mail dependency and unused imports

--- backend/src/main/java/edu/cit/binagatan/pirmaph/auth/application/PasswordRecoveryService.java
+++ backend/src/main/java/edu/cit/binagatan/pirmaph/auth/application/PasswordRecoveryService.java
@@ -6,8 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
